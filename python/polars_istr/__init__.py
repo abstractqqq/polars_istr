259bf384e39d6148a374778926f2e237bcec8bb2
abstractqqq <tq9695@gmail.com>
2024-04-09 11:58:22 -0400

    less targets, bump ver to 0.1

--- python/polars_istr/__init__.py
+++ python/polars_istr/__init__.py
@@ -3,5 +3,5 @@ from .isin import IsinExt  # noqa: E402
 from .cusip import CusipExt  # noqa: E402
 from .url import UrlExt  # noqa: E402
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 __all__ = ["IbanExt", "IsinExt", "CusipExt", "UrlExt"]
