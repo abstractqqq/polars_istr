22601728bb00178db1c37ac3b1b01833a4ae781b
abstractqqq <tq9695@gmail.com>
2024-05-04 16:16:29 -0400

    updated syntax

--- python/polars_istr/cusip.py
+++ python/polars_istr/cusip.py
@@ -1,8 +1,153 @@
 from __future__ import annotations
 import polars as pl
 from polars.utils.udfs import _get_shared_lib_location
+from ._utils import pl_plugin, StrOrExpr
 
 _lib = _get_shared_lib_location(__file__)
+
+
+def cusip_extract_all(x: StrOrExpr) -> pl.Expr:
+    """
+    Returns a struct containing country_code, issue_num, issuer_num, check_digit,
+    or null, if it cannot be parsed.
+
+    Country Code is null for valid CUSIPs which are not (extended) CINS
+    """
+    return pl_plugin(
+        args=[x],
+        lib=_lib,
+        symbol="pl_cusip_full",
+        is_elementwise=True,
+    )
+
+
+def cusip_issue_num(x: StrOrExpr) -> pl.Expr:
+    """
+    Returns the issue number from the CUSIP, or null if it cannot be parsed.
+    """
+    return pl_plugin(
+        args=[x],
+        lib=_lib,
+        symbol="pl_cusip_issue_num",
+        is_elementwise=True,
+    )
+
+
+def cusip_issuer_num(x: StrOrExpr) -> pl.Expr:
+    """
+    Returns the issuer number from the CUSIP, or null if it cannot be parsed.
+    """
+    return pl_plugin(
+        args=[x],
+        lib=_lib,
+        symbol="pl_cusip_issuer_num",
+        is_elementwise=True,
+    )
+
+
+def cusip_check_digit(x: StrOrExpr) -> pl.Expr:
+    """
+    Returns check digit from the CUSIP, or null if it cannot be parsed.
+    """
+    return pl_plugin(
+        args=[x],
+        lib=_lib,
+        symbol="pl_cusip_check_digit",
+        is_elementwise=True,
+    )
+
+
+def cusip_country_code(x: StrOrExpr) -> pl.Expr:
+    """
+    Returns the country code from the CUSIP, or null if it cannot be parsed.
+    """
+    return pl_plugin(
+        args=[x],
+        lib=_lib,
+        symbol="pl_cusip_country_code",
+        is_elementwise=True,
+    )
+
+
+def cusip_payload(x: StrOrExpr) -> pl.Expr:
+    """
+    Returns the payload (CUSIP ex. check digit) from the CUSIP, or null if it
+    cannot be parsed.
+    """
+    return pl_plugin(
+        args=[x],
+        lib=_lib,
+        symbol="pl_cusip_payload",
+        is_elementwise=True,
+    )
+
+
+def cusip_is_private_issue(x: StrOrExpr) -> pl.Expr:
+    """
+    Returns true if the issue number is reserved for private use.
+    """
+    return pl_plugin(
+        args=[x],
+        lib=_lib,
+        symbol="pl_cusip_is_private_issue",
+        is_elementwise=True,
+    )
+
+
+def cusip_has_private_issuer(x: StrOrExpr) -> pl.Expr:
+    """
+    Returns true if the issuer is reserved for private use.
+    """
+    return pl_plugin(
+        args=[x],
+        lib=_lib,
+        symbol="pl_cusip_has_private_issuer",
+        is_elementwise=True,
+    )
+
+
+def cusip_is_private_use(x: StrOrExpr) -> pl.Expr:
+    """
+    Returns True if either the issuer or issue number is reserved for
+    private use.
+    """
+    return pl_plugin(args=[x], lib=_lib, symbol="pl_cusip_is_private_use", is_elementwise=True)
+
+
+def cusip_is_cins(x: StrOrExpr) -> pl.Expr:
+    """
+    Returns true if this CUSIP number is actually a
+    CUSIP International Numbering System (CINS) number,
+    false otherwise (i.e., that it has a letter as the first character of its issuer number).
+    See also is_cins_base() and is_cins_extended().
+
+    Null if unable to parse.
+    """
+    return pl_plugin(args=[x], lib=_lib, symbol="pl_cusip_is_cins", is_elementwise=True)
+
+
+def cusip_is_cins_base(x: StrOrExpr) -> pl.Expr:
+    """
+    Returns true if this CUSIP identifier is actually a CUSIP International
+    Numbering System (CINS) identifier (with the further restriction that
+    it does not use `I`, `O` or `Z` as its country code), false otherwise.
+    See also is_cins() and is_cins_extended().
+
+    Null if unable to parse.
+    """
+    return pl_plugin(args=[x], lib=_lib, symbol="pl_cusip_is_cins_base", is_elementwise=True)
+
+
+def cusip_is_cins_extended(x: StrOrExpr) -> pl.Expr:
+    """
+    Returns true if this CUSIP identifier is actually a CUSIP International
+    Numbering System (CINS) identifier (with the further restriction that
+    it does not use `I`, `O` or `Z` as its country code), false otherwise.
+    See also is_cins() and is_cins_extended().
+
+    Null if unable to parse.
+    """
+    return pl_plugin(args=[x], lib=_lib, symbol="pl_cusip_is_cins_extended", is_elementwise=True)
 
 
 @pl.api.register_expr_namespace("cusip")
@@ -19,12 +164,6 @@ class CusipExt:
         self._expr: pl.Expr = expr
 
     def extract_all(self) -> pl.Expr:
-        """
-        Returns a struct containing country_code, issue_num, issuer_num, check_digit,
-        or null, if it cannot be parsed.
-
-        Country Code is null for valid CUSIPs which are not (extended) CINS
-        """
         return self._expr.register_plugin(
             lib=_lib,
             symbol="pl_cusip_full",
@@ -32,9 +171,6 @@ class CusipExt:
         )
 
     def issue_num(self) -> pl.Expr:
-        """
-        Returns the issue number from the CUSIP, or null if it cannot be parsed.
-        """
         return self._expr.register_plugin(
             lib=_lib,
             symbol="pl_cusip_issue_num",
@@ -42,9 +178,6 @@ class CusipExt:
         )
 
     def issuer_num(self) -> pl.Expr:
-        """
-        Returns the issuer number from the CUSIP, or null if it cannot be parsed.
-        """
         return self._expr.register_plugin(
             lib=_lib,
             symbol="pl_cusip_issuer_num",
@@ -52,9 +185,6 @@ class CusipExt:
         )
 
     def check_digit(self) -> pl.Expr:
-        """
-        Returns check digit from the CUSIP, or null if it cannot be parsed.
-        """
         return self._expr.register_plugin(
             lib=_lib,
             symbol="pl_cusip_check_digit",
@@ -62,9 +192,6 @@ class CusipExt:
         )
 
     def country_code(self) -> pl.Expr:
-        """
-        Returns the country code from the CUSIP, or null if it cannot be parsed.
-        """
         return self._expr.register_plugin(
             lib=_lib,
             symbol="pl_cusip_country_code",
@@ -72,10 +199,6 @@ class CusipExt:
         )
 
     def payload(self) -> pl.Expr:
-        """
-        Returns the payload (CUSIP ex. check digit) from the CUSIP, or null if it
-        cannot be parsed.
-        """
         return self._expr.register_plugin(
             lib=_lib,
             symbol="pl_cusip_payload",
@@ -83,9 +206,6 @@ class CusipExt:
         )
 
     def is_private_issue(self) -> pl.Expr:
-        """
-        Returns true if the issue number is reserved for private use.
-        """
         return self._expr.register_plugin(
             lib=_lib,
             symbol="pl_cusip_is_private_issue",
@@ -93,9 +213,6 @@ class CusipExt:
         )
 
     def has_private_issuer(self) -> pl.Expr:
-        """
-        Returns true if the issuer is reserved for private use.
-        """
         return self._expr.register_plugin(
             lib=_lib,
             symbol="pl_cusip_has_private_issuer",
@@ -103,47 +220,19 @@ class CusipExt:
         )
 
     def is_private_use(self) -> pl.Expr:
-        """
-        Returns True if either the issuer or issue number is reserved for
-        private use.
-        """
         return self._expr.register_plugin(
             lib=_lib, symbol="pl_cusip_is_private_use", is_elementwise=True
         )
 
     def is_cins(self) -> pl.Expr:
-        """
-        Returns true if this CUSIP number is actually a
-        CUSIP International Numbering System (CINS) number,
-        false otherwise (i.e., that it has a letter as the first character of its issuer number).
-        See also is_cins_base() and is_cins_extended().
-
-        Null if unable to parse.
-        """
         return self._expr.register_plugin(lib=_lib, symbol="pl_cusip_is_cins", is_elementwise=True)
 
     def is_cins_base(self) -> pl.Expr:
-        """
-        Returns true if this CUSIP identifier is actually a CUSIP International
-        Numbering System (CINS) identifier (with the further restriction that
-        it does not use ‘I’, ‘O’ or ‘Z’ as its country code), false otherwise.
-        See also is_cins() and is_cins_extended().
-
-        Null if unable to parse.
-        """
         return self._expr.register_plugin(
             lib=_lib, symbol="pl_cusip_is_cins_base", is_elementwise=True
         )
 
     def is_cins_extended(self) -> pl.Expr:
-        """
-        Returns true if this CUSIP identifier is actually a CUSIP International
-        Numbering System (CINS) identifier (with the further restriction that
-        it does not use ‘I’, ‘O’ or ‘Z’ as its country code), false otherwise.
-        See also is_cins() and is_cins_extended().
-
-        Null if unable to parse.
-        """
         return self._expr.register_plugin(
             lib=_lib, symbol="pl_cusip_is_cins_extended", is_elementwise=True
         )
--- python/polars_istr/type_alias.py
+++ python/polars_istr/type_alias.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
+from typing import Union
 import sys
+import polars as pl
 
 if sys.version_info >= (3, 10):
     from typing import TypeAlias  # noqa
 else:  # 3.9, 3.8
     from typing_extensions import TypeAlias  # noqa
+
+StrOrExpr: TypeAlias = Union[str, pl.Expr]
